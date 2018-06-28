33785ec358c3af706bd3dae5bbf47bb88cb8a44d
tedy <tedy@r2v.io>
2018-06-28 18:11:33 +0900

    Implementing Master Node Server & p2p server

--- client/src/Components/App/AppContainer.js
+++ client/src/Components/App/AppContainer.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { injectGlobal } from "styled-components";
 import reset from "styled-reset";
-// import axios from "axios";
+import axios from "axios";
 import typography from "../../typography";
-// import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE } from "../../constants";
-// import AppPresenter from "./AppPresenter";
-import logo from './ygg_symbol_shadow.png';
+import { MASTER_NODE, SELF_NODE, SELF_P2P_NODE } from "../../constants";
+import AppPresenter from "./AppPresenter";
+import logo from '../../assets/images/ygg_symbol_shadow.png';
 import './App.css';
 
 const baseStyles = () => injectGlobal`
@@ -17,20 +17,47 @@ const baseStyles = () => injectGlobal`
   }
 `;
 
-//todo : 마스터 노드에게 post request(마스터노드 등록)
 
+  
 class AppContainer extends Component {
-  render() {
-    baseStyles();
-    return (
-      <div className="AppContainer">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Yggdrash Wallet</h1>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        isLoading: true
+    };
+
+    static PropTypes = {
+        sharedPort: PropTypes.number.isRequired
+    };
+
+    componentDidMount = () => {
+        const { sharedPort } = this.props;
+        this._registerOnMaster(sharedPort);
+        this._getAddress();
+    };
+    render() {
+        baseStyles();
+        return (
+        <div className="AppContainer">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Yggdrash Wallet</h1>
+            </header>
+        </div>
+        // <AppPresenter {...this.state}/>
+        );
+    };
+
+    //todo : 마스터 노드에게 post request(마스터노드 등록)
+    _registerOnMaster = async port => {
+        
+        const request = await axios.post(`${MASTER_NODE}/peers`, {
+            peer: SELF_P2P_NODE(port)
+        });
+    };
+
+    _getAddress = async port =>{
+         const request = await axios.get(`${SELF_NODE(port)}/me/address`);
+         console.log(request.data)
+    }
 }
 
 export default AppContainer;
